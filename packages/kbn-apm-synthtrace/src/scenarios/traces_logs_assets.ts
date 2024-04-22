20181da441d842ca36a1cd83ba1d12863c6e1c9c
Caue Marcondes <caue.marcondes@elastic.co>
2024-04-22 13:03:27 +0100

    experiments

--- packages/kbn-apm-synthtrace/src/scenarios/traces_logs_assets.ts
+++ packages/kbn-apm-synthtrace/src/scenarios/traces_logs_assets.ts
@@ -10,9 +10,9 @@ import {
   ApmFields,
   generateLongId,
   generateShortId,
-  Instance,
   log,
-  Serializable,
+  LogDocument,
+  SynthtraceGenerator,
 } from '@kbn/apm-synthtrace-client';
 import { Readable } from 'stream';
 import { Scenario } from '../cli/scenario';
@@ -23,81 +23,81 @@ const ENVIRONMENT = getSynthtraceEnvironment(__filename);
 
 const scenario: Scenario<ApmFields> = async (runOptions) => {
   const { logger } = runOptions;
-  const { numServices = 3 } = runOptions.scenarioOpts || {};
 
   return {
     generate: ({ range, clients: { apmEsClient, assetsEsClient, logsEsClient } }) => {
+      const serviceName = 'my-service';
       const transactionName = '240rpm/75% 1000ms';
 
       const successfulTimestamps = range.interval('1m').rate(1);
       const failedTimestamps = range.interval('1m').rate(1);
-      const serviceNames = [...Array(numServices).keys()].map((index) => `synth-node-${index}`);
 
-      const instances = serviceNames.map((serviceName) =>
-        apm
-          .service({ name: serviceName, environment: ENVIRONMENT, agentName: 'nodejs' })
-          .instance('instance')
-      );
-      const instanceSpans = (instance: Instance) => {
-        const successfulTraceEvents = successfulTimestamps.generator((timestamp) =>
-          instance
-            .transaction({ transactionName })
-            .timestamp(timestamp)
-            .duration(1000)
-            .success()
-            .children(
-              instance
-                .span({
-                  spanName: 'GET apm-*/_search',
-                  spanType: 'db',
-                  spanSubtype: 'elasticsearch',
-                })
-                .duration(1000)
-                .success()
-                .destination('elasticsearch')
-                .timestamp(timestamp),
-              instance
-                .span({ spanName: 'custom_operation', spanType: 'custom' })
-                .duration(100)
-                .success()
-                .timestamp(timestamp)
-            )
-        );
+      const instance = apm
+        .service({
+          name: serviceName,
+          environment: ENVIRONMENT,
+          agentName: 'nodejs',
+        })
+        .instance('instance');
 
-        const failedTraceEvents = failedTimestamps.generator((timestamp) =>
-          instance
-            .transaction({ transactionName })
-            .timestamp(timestamp)
-            .duration(1000)
-            .failure()
-            .errors(
-              instance
-                .error({
-                  message: '[ResponseError] index_not_found_exception',
-                  type: 'ResponseError',
-                })
-                .timestamp(timestamp + 50)
-            )
-        );
-
-        const metricsets = range
-          .interval('30s')
-          .rate(1)
-          .generator((timestamp) =>
+      const successfulTraceEvents = successfulTimestamps.generator((timestamp) =>
+        instance
+          .transaction({ transactionName })
+          .timestamp(timestamp)
+          .duration(1000)
+          .success()
+          .children(
             instance
-              .appMetrics({
-                'system.memory.actual.free': 800,
-                'system.memory.total': 1000,
-                'system.cpu.total.norm.pct': 0.6,
-                'system.process.cpu.total.norm.pct': 0.7,
+              .span({
+                spanName: 'GET apm-*/_search',
+                spanType: 'db',
+                spanSubtype: 'elasticsearch',
               })
+              .duration(1000)
+              .success()
+              .destination('elasticsearch')
+              .timestamp(timestamp),
+            instance
+              .span({ spanName: 'custom_operation', spanType: 'custom' })
+              .duration(100)
+              .success()
               .timestamp(timestamp)
-          );
+          )
+      );
 
-        return [...successfulTraceEvents, ...failedTraceEvents, ...metricsets];
-      };
+      // const failedTraceEvents = failedTimestamps.generator((timestamp) =>
+      //   instance
+      //     .transaction({ transactionName })
+      //     .timestamp(timestamp)
+      //     .duration(1000)
+      //     .failure()
+      //     .errors(
+      //       instance
+      //         .error({
+      //           message: '[ResponseError] index_not_found_exception',
+      //           type: 'ResponseError',
+      //         })
+      //         .timestamp(timestamp + 50)
+      //     )
+      // );
 
-      const logsWithTraces = range
+      // const metricsets = range
+      //   .interval('30s')
+      //   .rate(1)
+      //   .generator((timestamp) =>
+      //     instance
+      //       .appMetrics({
+      //         'system.memory.actual.free': 800,
+      //         'system.memory.total': 1000,
+      //         'system.cpu.total.norm.pct': 0.6,
+      //         'system.process.cpu.total.norm.pct': 0.7,
+      //       })
+      //       .timestamp(timestamp)
+      //   );
+
+      // return [successfulTraceEvents];
+
+      const logs = range
         .interval('1m')
         .rate(1)
         .generator((timestamp) => {
@@ -118,14 +118,14 @@ const scenario: Scenario<ApmFields> = async (runOptions) => {
                 .create()
                 .message(message.replace('<random>', generateShortId()))
                 .logLevel(level)
-                .service(serviceNames[0])
+                .service(serviceName)
                 .defaults({
                   'trace.id': generateShortId(),
                   'agent.name': 'nodejs',
                   'orchestrator.cluster.name': CLUSTER.clusterName,
                   'orchestrator.cluster.id': CLUSTER.clusterId,
                   'orchestrator.namespace': CLUSTER.namespace,
-                  'container.name': `${serviceNames[0]}-${generateShortId()}`,
+                  'container.name': `${serviceName}-${generateShortId()}`,
                   'orchestrator.resource.id': generateShortId(),
                   'cloud.provider': 'gcp',
                   'cloud.region': 'eu-central-1',
@@ -138,73 +138,36 @@ const scenario: Scenario<ApmFields> = async (runOptions) => {
             });
         });
 
-      const logsOnly = range
-        .interval('1m')
-        .rate(1)
-        .generator((timestamp) => {
-          return Array(3)
-            .fill(0)
-            .map(() => {
-              const { message, level } = {
-                message: 'A simple log with something random <random> in the middle',
-                level: 'info',
-              };
-              const CLUSTER = {
-                clusterId: generateShortId(),
-                clusterName: 'synth-cluster-2',
-                namespace: 'production',
-              };
-
-              return log
-                .create()
-                .message(message.replace('<random>', generateShortId()))
-                .logLevel(level)
-                .service('synth-java')
-                .defaults({
-                  'trace.id': generateShortId(),
-                  'agent.name': 'nodejs',
-                  'orchestrator.cluster.name': CLUSTER.clusterName,
-                  'orchestrator.cluster.id': CLUSTER.clusterId,
-                  'orchestrator.namespace': CLUSTER.namespace,
-                  'container.name': `synth-java-${generateShortId()}`,
-                  'orchestrator.resource.id': generateShortId(),
-                  'cloud.provider': 'gcp',
-                  'cloud.region': 'eu-central-1',
-                  'cloud.availability_zone': 'eu-central-1a',
-                  'cloud.project.id': generateShortId(),
-                  'cloud.instance.id': generateShortId(),
-                  'log.file.path': `/logs/${generateLongId()}/error.txt`,
-                })
-                .timestamp(timestamp);
-            });
-        });
-
-      function* createGeneratorFromArray(arr: Array<Serializable<any>>) {
-        yield* arr;
+      function* copyGenerator(gen: SynthtraceGenerator<LogDocument | ApmFields>) {
+        for (const value of gen) {
+          yield value;
+        }
       }
 
-      const logsValuesArray = [...logsWithTraces, ...logsOnly];
-      const logsGen = createGeneratorFromArray(logsValuesArray);
-      const logsGenAssets = createGeneratorFromArray(logsValuesArray);
+      function cloneGenerator(original: SynthtraceGenerator<LogDocument | ApmFields>) {
+        const clone = Object.assign(Object.create(Object.getPrototypeOf(original)), original);
+        clone[Symbol.iterator] = function* () {
+          yield* original;
+        };
+        return clone;
+      }
 
-      const traces = instances.flatMap((instance) => instanceSpans(instance));
-      const tracesGen = createGeneratorFromArray(traces);
-      const tracesGenAssets = createGeneratorFromArray(traces);
+      const copyArray = Readable.from(Array.from(logs).concat(Array.from(successfulTraceEvents)));
+      // cloneGenerator(Array.from(logs)),
+      // cloneGenerator(Array.from(successfulTraceEvents)),
 
       return [
         withClient(
-          assetsEsClient,
-          logger.perf('generating_assets_events', () =>
-            Readable.from(Array.from(logsGenAssets).concat(Array.from(tracesGenAssets)))
-          )
-        ),
-        withClient(
           logsEsClient,
-          logger.perf('generating_logs', () => logsGen)
+          logger.perf('generating_logs', () => logs)
         ),
         withClient(
           apmEsClient,
-          logger.perf('generating_apm_events', () => tracesGen)
+          logger.perf('generating_apm_events', () => successfulTraceEvents)
+        ),
+        withClient(
+          assetsEsClient,
+          logger.perf('generating_assets_events', () => copyArray)
         ),
       ];
     },
