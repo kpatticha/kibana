a812ca43d0be468991486921cad9974a4e0761c4
Kate Patticha <aikaterini.patticha@elastic.co>
2024-07-10 18:51:28 +0300

    wip

--- x-pack/plugins/observability_solution/apm/server/lib/helpers/create_es_client/create_assets_es_client/create_assets_es_clients.ts
+++ x-pack/plugins/observability_solution/apm/server/lib/helpers/create_es_client/create_assets_es_client/create_assets_es_clients.ts
@@ -10,7 +10,7 @@ import { ElasticsearchClient } from '@kbn/core/server';
 import { unwrapEsResponse } from '@kbn/observability-plugin/common/utils/unwrap_es_response';
 import { withApmSpan } from '../../../../utils/with_apm_span';
 
-const ENTITIES_INDEX_NAME = '.entities-observability.latest-*';
+const ENTITIES_INDEX_NAME = '.entities.v1.latest.builtin_services*';
 
 export function cancelEsRequestOnAbort<T extends Promise<any>>(
   promise: T,
